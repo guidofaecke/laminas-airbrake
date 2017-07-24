d6a9340e57c87d67e491875efdd3c60660928351
Frank Houweling <fhouweling@senet.nl>
2017-07-24 21:28:07 +0200

    no message

---
+++ src/Module.php
@@ -0,0 +1,24 @@
+<?php
+/*
+* Copyright (C) Senet Eindhoven B.V. - All Rights Reserved
+* Unauthorized copying of this file, via any medium is strictly prohibited
+* Proprietary and confidential
+* Written by Frank Houweling <fhouweling@senet.nl>, 7/24/2017
+*/
+
+namespace FrankHouweling\ZendAirbrake;
+
+/**
+ * Class Module
+ * @package FrankHouweling\ZendAirbrake
+ */
+class Module
+{
+    /**
+     * @return array
+     */
+    public function getConfig()
+    {
+        return [];
+    }
+}
