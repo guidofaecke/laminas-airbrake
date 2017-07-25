5e1ad9a00541fd22018f3e00841b768da4741cee
Frank Houweling <houweling.frank@gmail.com>
2017-07-25 13:14:25 +0200

    Start with unittesting

---
+++ test/ZendframeworkTriggerTest.php
@@ -0,0 +1,16 @@
+<?php
+/*
+* Copyright (C) Senet Eindhoven B.V. - All Rights Reserved
+* Unauthorized copying of this file, via any medium is strictly prohibited
+* Proprietary and confidential
+* Written by Frank Houweling <fhouweling@senet.nl>, 7/25/2017
+*/
+
+
+namespace FrankHouweling\ZendAirbrakeTest;
+
+
+class ZendframeworkTriggerTest extends TestCase
+{
+
+}
